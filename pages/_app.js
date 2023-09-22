f73fd7699023d2cda886612ea067d35912168f6d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-22 15:17:57 +0100

    wrapped our _app.js in Provider and PersistGate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "../styles/globals.scss";
+// so we are going to wrap our app into provider store that will feed all the stored info. we need to import a few packages
+import { Provider } from "react-redux";
+import store from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+//we start by declaring our persistor that will persist our store
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  //here we are wrapping our entire app with the provider and we are passing it store which equals to the store we have set up previously
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
