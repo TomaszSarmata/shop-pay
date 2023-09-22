3031e923035c2ef7571f232ce1a6db705755ee73
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-22 14:14:07 +0100

    changed the default export in helper functions in utils to be able to export many functions in just one object db. Also tested disconnect and connect in our api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import db from "../../utils/db";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  db.connectDb(); //here and below we are simply testing if it works and the message should be seen in VS terminal
+  db.disconnectDb();
+  res.status(200).json({ name: "John Doe" });
 }
