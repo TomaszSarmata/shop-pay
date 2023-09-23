82bfeb0f58904bb381cbfff22bd160c8fba21bf4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-23 07:48:45 +0100

    Created components folder, inside created header folder wiht index.js and also styles.module.scss and imported styles.module to our index.js

---
+++ components/header/index.js
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss"; //here we are only importing scss modules for this particular component
+
+export default function Header() {
+  return <div>Header</div>;
+}
