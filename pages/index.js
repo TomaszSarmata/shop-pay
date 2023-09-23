7bee9b05cf94aa66e25d5cf22a743168941cdb0e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-23 07:57:39 +0100

    include Header component on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Header from "../components/header";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <div>
+      <Header></Header>
       <h1 className={styles.red}>Welcome</h1>
       <button>click me</button>
     </div>
