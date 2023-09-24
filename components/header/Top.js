78a5b180e97b8e5ff81fe711fdf83937610d914d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-24 11:13:53 +0100

    created Top component and imported it to the header index

---
+++ components/header/Top.js
@@ -0,0 +1,4 @@
+import styles from "./styles.module.scss";
+export default function Top() {
+  return <div>Top</div>;
+}
--- components/header/index.js
+++ components/header/index.js
@@ -1,10 +1,12 @@
 import styles from "./styles.module.scss"; //here we are only importing scss modules for this particular component
 import Ad from "./Ad";
+import Top from "./Top";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Ad></Ad>
+      <Top></Top>
     </header>
   );
 }
