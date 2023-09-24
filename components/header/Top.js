01d7293b8a3a0d3deb8127ed9f0921f3583032b5
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-24 11:19:32 +0100

    added some classes and more content to the Top component

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,4 +1,13 @@
 import styles from "./styles.module.scss";
 export default function Top() {
-  return <div>Top</div>;
+  return (
+    <div className={styles.top}>
+      <div className={styles.top_container}>
+        <div></div>
+        <ul className="styles top_list">
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
