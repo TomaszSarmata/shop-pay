2d4be21d7a3efaf72fb10efdf10c1ba2ae000447
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-26 14:54:56 +0100

    added classes with content to Footer compnent

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,11 @@
 import styles from "./styles.module.scss";
+import Links from "./Links";
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_container}>
+        <Links></Links>
+      </div>
+    </footer>
+  );
 }
