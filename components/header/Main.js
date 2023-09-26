22ab60c8f240b90b493381ba05adf573a07b25cb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-26 11:33:15 +0100

    pulled out the cart value from react-redux to display for the user the amount of products in the cart

--- components/header/Main.js
+++ components/header/Main.js
@@ -3,7 +3,10 @@ import { RiSearch2Line } from "react-icons/ri";
 import { FaOpencart } from "react-icons/fa";
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 export default function Main() {
+  const { cart } = useSelector((state) => ({ ...state })); //here we are pulling out the value of the cart from react-redux and we are going to use cart.length (as its a array) to display the number of products in the cart
+  console.log("here", cart.length);
   return (
     <div className={styles.main}>
       <div className={styles.main_container}>
@@ -22,6 +25,7 @@ export default function Main() {
         <Link legacyBehavior href="/cart">
           <a className="styles.cart">
             <FaOpencart className={styles.react_icon} />
+            <span>{cart.length}</span>
           </a>
         </Link>
       </div>
