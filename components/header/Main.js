eb838fb30b32ec904b36ff1d09e168d5d56222fb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-26 11:14:08 +0100

    added content to the Main component and some react icons where I had to set the width as they were taking up the entire width

--- components/header/Main.js
+++ components/header/Main.js
@@ -1,4 +1,30 @@
+import { RiSearch2Line } from "react-icons/ri";
+
+import { FaOpencart } from "react-icons/fa";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.main_container}>
+        <Link legacyBehavior href="/">
+          <a className={styles.logo}>
+            <img src="/logo.png" alt="logo" />
+          </a>
+        </Link>
+        <div className={styles.search}>
+          <input type="text" placeholder="Search..." />
+          <div className={styles.search_icon}>
+            <RiSearch2Line className={styles.react_icon} />
+          </div>
+        </div>
+
+        <Link legacyBehavior href="/cart">
+          <a className="styles.cart">
+            <FaOpencart className={styles.react_icon} />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
