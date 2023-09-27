a69a89611f1c7a559e098a5217af4fbcd9c9ba9f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-27 06:23:23 +0100

    created Socials component and imported into index.js

---
+++ components/footer/Socials.js
@@ -0,0 +1,55 @@
+import styles from "./styles.module.scss";
+import { FaFacebookF, FaTiktok } from "react-icons/fa";
+import {
+  BsInstagram,
+  BsTwitter,
+  BsYoutube,
+  BsPinterest,
+  BsSnapchat,
+} from "react-icons/bs";
+export default function Socials() {
+  return (
+    <div className={styles.footer_socials}>
+      <section>
+        <h1>STAY CONNECTED</h1>
+        <ul>
+          <li>
+            <a href="/" target="_blank">
+              <FaFacebookF />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <BsInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <BsTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <BsYoutube />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <BsPinterest />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <BsSnapchat />
+            </a>
+          </li>
+          <li>
+            <a href="/" target="_blank">
+              <FaTiktok />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,10 +1,12 @@
 import styles from "./styles.module.scss";
 import Links from "./Links";
+import Socials from "./Socials";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
         <Links></Links>
+        <Socials></Socials>
       </div>
     </footer>
   );
