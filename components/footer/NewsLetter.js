ca17ce79ade9d71be43fce24a009bd0ef1988f76
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-27 06:35:30 +0100

    created NewsLetter component and added to index.js

---
+++ components/footer/NewsLetter.js
@@ -0,0 +1,8 @@
+import styles from "./styles.module.scss";
+export default function NewsLetter() {
+  return (
+    <div className={styles.newsletter}>
+      <h3>SIGN UP FOR OUR NEWSLETTER</h3>
+    </div>
+  );
+}
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,12 +1,14 @@
 import styles from "./styles.module.scss";
 import Links from "./Links";
 import Socials from "./Socials";
+import NewsLetter from "./NewsLetter";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
         <Links></Links>
         <Socials></Socials>
+        <NewsLetter></NewsLetter>
       </div>
     </footer>
   );
