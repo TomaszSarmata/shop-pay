cc9d81d77d218ac1f35d78ec152c60c5c896cb3d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-27 16:39:37 +0100

    Added more settings to nextauth file in order to be able to create a new page for the sign in. Also created a new JWT_SECRET password in .env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,4 +19,12 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  //here we are sinply adding a page to the nextauth settings with the route /signin
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt", //we are changing the strategy to use json web tokens. Web token will hold encrypted info about the user. To decrypt it we will also need 'secret' as below
+  },
+  secret: process.env.JWT_SECRET, //JWT_SECRET is set up in the .env file
 });
