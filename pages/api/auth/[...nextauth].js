4f143d6fa9dbf9bd6a8c7fc08d73a431346f5942
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-30 18:31:56 +0100

    added twitter provider to allow users log in with x account

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import AppleProvider from "next-auth/providers/apple";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
 
@@ -14,9 +15,10 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // OAuth authentication providers...
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET,
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: "2.0", // opt-in to Twitter OAuth 2.0
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
