73e5003da2dd36e0c4dcbc4dbea9733e3ace3fee
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-01 15:19:38 +0100

    imported header, footer and styles to sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import styles from "../styles/signin.module.scss";
 
 export default function signin() {
-  return <div>signin</div>;
+  return (
+    <>
+      <Header country="UK"></Header>
+      Page itself
+      <Footer country="UK"></Footer>
+    </>
+  );
 }
