0c6b476e5a468632e580db91ec66d5c3332ec3d2
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-02 15:00:48 +0100

    added some content to signin.js

--- pages/signin.js
+++ pages/signin.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import styles from "../styles/signin.module.scss";
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 export default function signin() {
   return (
     <>
       <Header country="UK"></Header>
-      Page itself
+      <div className={styles.login}>
+        <div className={styles.login_container}>
+          <div className={styles.login_header}>
+            <div className={styles.back_svg}>
+              <BiLeftArrowAlt></BiLeftArrowAlt>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer country="UK"></Footer>
     </>
   );
