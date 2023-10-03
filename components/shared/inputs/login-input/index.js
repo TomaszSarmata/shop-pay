823ad8d146498a4d966f04f910618882aabe0146
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-03 08:56:12 +0100

    passed the remaining props to the LoginInput and extracted field and meta from the formik

--- components/shared/inputs/login-input/index.js
+++ components/shared/inputs/login-input/index.js
@@ -2,8 +2,10 @@ import styles from "./styles.module.scss";
 import { BiUser } from "react-icons/bi";
 import { IoKeyOutline } from "react-icons/io5";
 import { SiMinutemailer } from "react-icons/si";
+import { useField } from "formik";
 
-export default function LoginInput({ icon, placeholder }) {
+export default function LoginInput({ icon, placeholder, ...props }) {
+  const [field, meta] = useField();
   return (
     <div className={styles.input}>
       {icon == "user" ? (
