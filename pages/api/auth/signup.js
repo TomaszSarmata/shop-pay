27c6fa8d9053c4bf9155282fe6fcc7ee9ad0c59c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-05 11:15:51 +0100

    created an endpoint where we are going to first establish connection to the db and tehn make sure we have values for the name, email and password, and then we will validate all values

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,19 @@
+import nc from "next-connect";
+import db from "../../../utils/db";
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+  try {
+    await db.connectDb();
+    //below we are going to extract the name, email and password form the user input sent as a request as we want to make sure that none of them are null
+    const { name, email, password } = req.body; //we are getting that info from the body of the request
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default handler;
