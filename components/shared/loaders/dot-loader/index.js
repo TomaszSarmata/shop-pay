ec0499eb6b9cd8e2094912cb49db7db698c92b90
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-10 10:17:15 +0100

    imported DotsLoader into signin page

--- components/shared/loaders/dot-loader/index.js
+++ components/shared/loaders/dot-loader/index.js
@@ -1,5 +1,11 @@
 import styles from "./styles.module.scss";
+import DotLoader from "react-spinners/DotLoader";
 
-export default function DotLoader({ loading }) {
-  return <div>dotLoader</div>;
+export default function DotsLoader({ loading }) {
+  //if leading true then it will show up
+  return (
+    <div className={styles.loader}>
+      <DotLoader color="#2f82ff" loading={loading}></DotLoader>
+    </div>
+  );
 }
