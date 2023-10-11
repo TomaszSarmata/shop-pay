af1c35a1a7a7dfb31099b0c4a9337506c6f115a6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-11 16:17:43 +0100

    amended the paths for the payment images to ensure absolute paths rather than relatives as the images where not displaying

--- components/footer/Payment.js
+++ components/footer/Payment.js
@@ -4,9 +4,9 @@ export default function Payment() {
     <div className={styles.footer_payment}>
       <h3>WE ACCEPT</h3>
       <div className={styles.footer_flexwrap}>
-        <img src="images/payment/visa.webp" alt="visa" />
-        <img src="images/payment/mastercard.webp" alt="mastercard" />
-        <img src="images/payment/paypal.webp" alt="paypal" />
+        <img src="/images/payment/visa.webp" alt="visa" />
+        <img src="/images/payment/mastercard.webp" alt="mastercard" />
+        <img src="/images/payment/paypal.webp" alt="paypal" />
       </div>
     </div>
   );
