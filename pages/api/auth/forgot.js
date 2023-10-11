79b3f5fcc912c22c2b99f938fa2c05d0b8d3e0d0
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-11 16:56:50 +0100

    got rid off the unnecessary stuff and added a simple check for the email user to see if it exists in our db

--- pages/api/auth/forgot.js
+++ pages/api/auth/forgot.js
@@ -12,38 +12,11 @@ handler.post(async (req, res) => {
   try {
     await db.connectDb();
 
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: "Please fill in all fields" });
+    const { email } = req.body; //so thats coming from the browser or postman. So we gonna try to find the user with that email in our database.
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "This email does not exist." });
     }
-
-    if (!validateEmail(email)) {
-      return res.status(400).json({
-        message: "Invalid email",
-      });
-    }
-
-    const user = await User.findOne({ email: email });
-    if (user) {
-      return res.status(400).json({
-        message:
-          "This email is already linked to another user. If you think you have registered previously, try logging in using third party provider (i.e. Google, Twitter, Github)",
-      });
-    }
-
-    if (password.length < 6) {
-      return res.status(400).json({
-        message: "Your password must be at least 6 characters long",
-      });
-    }
-
-    const cryptedPassword = await bcrypt.hash(password, 12);
-    const newUser = new User({ name, email, password: cryptedPassword });
-    const addedUser = await newUser.save();
-    const activation_token = createActivationToken({
-      id: addedUser._id.toString(),
-    });
-
     const url = `${process.env.BASE_URL}/activate/${activation_token}`;
     sendEmail(email, url, "", "Activate your account.");
     await db.disconnectDb();
