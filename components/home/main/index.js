bf5a843ae0c17b831bf80b7d639c1e82c376cf9a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-15 18:05:30 +0100

    created a component called MainSwiper and imported it into the main of the homepage

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,10 +1,11 @@
 import styles from "./styles.module.scss";
+import MainSwiper from "./swiper";
 export default function Main() {
   return (
     <div className={styles.main}>
       <div className={styles.header}>header</div>
       <div className={styles.menu}>menu</div>
-      <div className={styles.swiper}>swiper</div>
+      <MainSwiper></MainSwiper>
       <div className={styles.offers}>offers</div>
       <div className={styles.user}>user</div>
     </div>
---
+++ components/home/main/swiper.js
@@ -0,0 +1,4 @@
+import styles from "./styles.module.scss";
+export default function MainSwiper() {
+  return <div className={styles.swiper}>swiper</div>;
+}
