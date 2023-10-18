aa2507751f35b49af7ba2c95dca7dc937745ac6d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-18 10:35:32 +0100

    Created menu component in the main of the home component and imported it into the main

---
+++ components/home/main/Menu.js
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import { menuArray } from "../../../data/home";
+import styles from "./styles.module.scss";
+
+import {
+  GiLargeDress,
+  GiClothes,
+  Gi3DHammer,
+  GiWatch,
+  GiBallerinaShoes,
+  GiHeadphones,
+  GiHealthCapsule,
+  GiSportMedal,
+  GiBigDiamondRing,
+} from "react-icons/gi";
+import { MdOutlineSportsEsports, MdOutlineSmartToy } from "react-icons/md";
+import { BiCameraMovie, BiGift, BiCategory } from "react-icons/bi";
+import { FaBaby } from "react-icons/fa";
+import { HiOutlineHome } from "react-icons/hi";
+import { AiOutlineSecurityScan } from "react-icons/ai";
+import { BsPhoneVibrate } from "react-icons/bs";
+export default function Menu() {
+  console.log("here");
+  console.log(menuArray);
+  return (
+    <div className={styles.menu}>
+      <ul>
+        <li>
+          <a className={styles.menu_header} href="">
+            <BiCategory></BiCategory>
+            <b>Categories</b>
+          </a>
+        </li>
+        <li className={styles.menu_list}>
+          {menuArray.map((item, i) => (
+            <Link key={i} href={item.link} legacyBehavior>
+              <a>
+                {i == 0 ? (
+                  <GiLargeDress />
+                ) : i == 1 ? (
+                  <GiClothes />
+                ) : i == 2 ? (
+                  <GiHeadphones />
+                ) : i == 3 ? (
+                  <GiWatch />
+                ) : i == 4 ? (
+                  <HiOutlineHome />
+                ) : i == 5 ? (
+                  <GiHealthCapsule />
+                ) : i == 6 ? (
+                  <GiBallerinaShoes />
+                ) : i == 7 ? (
+                  <GiBigDiamondRing />
+                ) : i == 8 ? (
+                  <GiSportMedal />
+                ) : i == 9 ? (
+                  <FaBaby />
+                ) : i == 10 ? (
+                  <BiCameraMovie />
+                ) : i == 11 ? (
+                  <MdOutlineSportsEsports />
+                ) : i == 12 ? (
+                  <BsPhoneVibrate />
+                ) : i == 13 ? (
+                  <MdOutlineSmartToy />
+                ) : i == 14 ? (
+                  <BiGift />
+                ) : i == 15 ? (
+                  <Gi3DHammer />
+                ) : i == 16 ? (
+                  <AiOutlineSecurityScan />
+                ) : (
+                  ""
+                )}
+                <span>{item.name}</span>
+              </a>
+            </Link>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
