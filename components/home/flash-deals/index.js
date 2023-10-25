e9ed1fc7ec95de18826dc1d56519773bfd33bcf4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-25 11:50:55 +0100

    added swiper to the main component and implemented a function to make it fully responsive

--- components/home/flash-deals/index.js
+++ components/home/flash-deals/index.js
@@ -1,7 +1,46 @@
 import { MdFlashOn } from "react-icons/md";
 import styles from "./styles.module.scss";
 import Countdown from "../../../components/countdown";
+import { useRef, useState, useEffect } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { flashDealsArray } from "../../../data/home";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import FlashCard from "./FlashCard";
+
 export default function FlashDeals() {
+  const [slidesPerView, setSlidesPerView] = useState(5);
+
+  useEffect(() => {
+    function updateSlidesPerView() {
+      if (window.innerWidth >= 1200) {
+        setSlidesPerView(5);
+      } else if (window.innerWidth >= 1000) {
+        setSlidesPerView(4);
+      } else if (window.innerWidth >= 700) {
+        setSlidesPerView(3);
+      } else if (window.innerWidth >= 600) {
+        setSlidesPerView(2);
+      } else {
+        setSlidesPerView(1);
+      }
+    }
+
+    updateSlidesPerView();
+
+    window.addEventListener("resize", updateSlidesPerView);
+
+    return () => {
+      window.removeEventListener("resize", updateSlidesPerView);
+    };
+  }, []);
+
   return (
     <div className={styles.flashDeals}>
       <div className={styles.flashDeals_header}>
@@ -11,6 +50,21 @@ export default function FlashDeals() {
         </h1>
         <Countdown></Countdown>
       </div>
+      <Swiper
+        slidesPerView={slidesPerView}
+        spaceBetween={30}
+        navigation={true}
+        modules={[Navigation]}
+        className="flashDeals_swiper"
+      >
+        <div className={styles.flashDeals_list}>
+          {flashDealsArray.map((product, index) => (
+            <SwiperSlide key={index}>
+              <FlashCard product={product}></FlashCard>
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
     </div>
   );
 }
