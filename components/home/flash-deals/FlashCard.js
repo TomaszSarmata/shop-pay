e0fb26bdbe647c1310de4dd218d49c28b65cb157
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-25 11:51:32 +0100

    created flashcard component and added some styling

---
+++ components/home/flash-deals/FlashCard.js
@@ -0,0 +1,36 @@
+import { MdFlashOn } from "react-icons/md";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+export default function FlashCard({ product }) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card_img}>
+        <Link href={product.link}>
+          <img src={product.image} alt="" />
+        </Link>
+        <div className={styles.flash}></div>
+      </div>
+      <div className={styles.card_price}>
+        <MdFlashOn></MdFlashOn>
+        <span>-{product.discount}%</span>
+        <br />
+        <span>
+          USD{(product.price - product.price / product.discount).toFixed(2)} $
+        </span>
+        <br />
+        <span>
+          -USD
+          {(
+            product.price -
+            (product.price - product.price / product.discount)
+          ).toFixed(2)}
+        </span>
+      </div>
+      <div className={styles.card_bar}>
+        <div className={styles.card_bar_inner} style={{ width: "75%" }}></div>
+      </div>
+      <div className={styles.card_percentage}>{product.sold}%</div>
+    </div>
+  );
+}
