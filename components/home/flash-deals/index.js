a3da40d1986616dbd1bf81eebf2ce3c0872e4c2f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-10-31 11:51:38 +0000

    commented out the code for swiper responsiveness using useEffect as applied breakpoints within the swiper itself

--- components/home/flash-deals/index.js
+++ components/home/flash-deals/index.js
@@ -15,31 +15,31 @@ import { Navigation } from "swiper/modules";
 import FlashCard from "./FlashCard";
 
 export default function FlashDeals() {
-  const [slidesPerView, setSlidesPerView] = useState(5);
+  // const [slidesPerView, setSlidesPerView] = useState(5);
 
-  useEffect(() => {
-    function updateSlidesPerView() {
-      if (window.innerWidth >= 1200) {
-        setSlidesPerView(5);
-      } else if (window.innerWidth >= 1000) {
-        setSlidesPerView(4);
-      } else if (window.innerWidth >= 700) {
-        setSlidesPerView(3);
-      } else if (window.innerWidth >= 600) {
-        setSlidesPerView(2);
-      } else {
-        setSlidesPerView(1);
-      }
-    }
+  // useEffect(() => {
+  //   function updateSlidesPerView() {
+  //     if (window.innerWidth >= 1200) {
+  //       setSlidesPerView(5);
+  //     } else if (window.innerWidth >= 1000) {
+  //       setSlidesPerView(4);
+  //     } else if (window.innerWidth >= 700) {
+  //       setSlidesPerView(3);
+  //     } else if (window.innerWidth >= 600) {
+  //       setSlidesPerView(2);
+  //     } else {
+  //       setSlidesPerView(1);
+  //     }
+  //   }
 
-    updateSlidesPerView();
+  //   updateSlidesPerView();
 
-    window.addEventListener("resize", updateSlidesPerView);
+  //   window.addEventListener("resize", updateSlidesPerView);
 
-    return () => {
-      window.removeEventListener("resize", updateSlidesPerView);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("resize", updateSlidesPerView);
+  //   };
+  // }, []);
 
   return (
     <div className={styles.flashDeals}>
@@ -51,11 +51,25 @@ export default function FlashDeals() {
         <Countdown></Countdown>
       </div>
       <Swiper
-        slidesPerView={slidesPerView}
+        slidesPerView={1} //{slidesPerView} that was with the useEffect
         spaceBetween={30}
         navigation={true}
         modules={[Navigation]}
         className="flashDeals_swiper"
+        breakpoints={{
+          500: {
+            slidesPerView: 2,
+          },
+          700: {
+            slidesPerView: 3,
+          },
+          900: {
+            slidesPerView: 4,
+          },
+          1100: {
+            slidesPerView: 5,
+          },
+        }}
       >
         <div className={styles.flashDeals_list}>
           {flashDealsArray.map((product, index) => (
