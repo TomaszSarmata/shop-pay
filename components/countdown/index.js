ddc7001c26948a880ce95abba1f06a1b8f85c9be
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-01 10:03:49 +0000

    created implemented countdown component to replace static values.

--- components/countdown/index.js
+++ components/countdown/index.js
@@ -1,5 +1,32 @@
 import styles from "./styles.module.scss";
-export default function Countdown() {
+import { useState, useEffect } from "react";
+import { calculateTimeDiff } from "./utils";
+
+const defaultremainigTime = {
+  seconds: "00",
+  minutes: "00",
+  hours: "00",
+  days: "00",
+};
+
+export default function Countdown({ date }) {
+  const [timeInMs, setTimeInMs] = useState(date.getTime());
+  const [remainingTime, setRemainingTime] = useState(defaultremainigTime);
+  console.log("ramianing time", remainingTime);
+  useEffect(() => {
+    setTimeInMs(date.getTime());
+  }, [date]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateRemainingTime(timeInMs);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeInMs]);
+  const updateRemainingTime = (timeInMs) => {
+    setRemainingTime(calculateTimeDiff(timeInMs));
+  };
+
   return (
     <div className={styles.countdown}>
       <span>1</span>
