f1b15108db8374fbf775f822073d54a850e42820
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-01 10:04:43 +0000

    created a helper function in the utils file for the countdown component that will calculate how many sec, min, hours and days are left to display for the user

---
+++ components/countdown/utils.js
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+
+export function calculateTimeDiff(timeInMs) {
+  const timestampDayjs = dayjs(timeInMs);
+  const nowDayjs = dayjs();
+
+  if (timestampDayjs.isBefore(nowDayjs)) {
+    return {
+      seconds: "00",
+      minutes: "00",
+      hours: "00",
+      days: "00",
+    };
+  } else {
+    return {
+      seconds: getRemainingSeconds(nowDayjs, timestampDayjs),
+      minutes: getRemainingMinutes(nowDayjs, timestampDayjs),
+      hours: getRemainingHours(nowDayjs, timestampDayjs),
+      days: getReaminingDays(nowDayjs, timestampDayjs),
+    };
+  }
+}
+
+function getRemainingSeconds(nowDayjs, timestampDayjs) {
+  const seconds = timestampDayjs.diff(nowDayjs, "seconds") % 60;
+  return seconds;
+}
+function getRemainingMinutes(nowDayjs, timestampDayjs) {
+  const minutes = timestampDayjs.diff(nowDayjs, "minutes") % 60;
+  return minutes;
+}
+function getRemainingHours(nowDayjs, timestampDayjs) {
+  const hours = timestampDayjs.diff(nowDayjs, "hours") % 24;
+  return hours;
+}
+function getReaminingDays(nowDayjs, timestampDayjs) {
+  const days = timestampDayjs.diff(nowDayjs, "days");
+  return days;
+}
