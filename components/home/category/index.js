0867f36ed790fe02771ec0db2b747ad941d518da
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-03 11:03:24 +0000

    implemented useMediaQuery from react-responsive to limit the number of photos displayed in a grid depending on the width of the screen

--- components/home/category/index.js
+++ components/home/category/index.js
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import { BsArrowRightCircle } from "react-icons/bs";
 import { useMediaQuery } from "react-responsive";
 export default function Category({ header, products, background }) {
+  const isMedium = useMediaQuery({ query: "(max-width: 1400px)" });
   return (
     <div className={styles.category} style={{ background: `${background}` }}>
       <div className={styles.category_header}>
@@ -9,7 +10,7 @@ export default function Category({ header, products, background }) {
         <BsArrowRightCircle></BsArrowRightCircle>
       </div>
       <div className={styles.category_products}>
-        {products.map((product, i) => (
+        {products.slice(0, isMedium ? 4 : 6).map((product, i) => (
           <div key={i} className={styles.product}>
             <img src={product.image} alt="" />
           </div>
