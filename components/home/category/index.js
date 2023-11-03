730fedfdcbdd16dc6c0ea9c195835856e0e4540a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-03 08:23:06 +0000

    added icon from react-icons, also extracted header and products from the parent

--- components/home/category/index.js
+++ components/home/category/index.js
@@ -1,4 +1,13 @@
 import styles from "./styles.module.scss";
-export default function Category() {
-  return <div>Category</div>;
+import { BsArrowRightCircle } from "react-icons/bs";
+export default function Category({ header, products }) {
+  return (
+    <div className={styles.category}>
+      <div className={styles.category_header}>
+        <h1>{header}</h1>
+        <BsArrowRightCircle></BsArrowRightCircle>
+      </div>
+      <div className={styles.category_products}></div>
+    </div>
+  );
 }
