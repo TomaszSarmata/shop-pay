0056cd9639399080c406ef66bfdfaba7a8e2cf05
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-03 08:59:31 +0000

    extracted background from the parent

--- components/home/category/index.js
+++ components/home/category/index.js
@@ -1,13 +1,17 @@
 import styles from "./styles.module.scss";
 import { BsArrowRightCircle } from "react-icons/bs";
-export default function Category({ header, products }) {
+export default function Category({ header, products, background }) {
   return (
-    <div className={styles.category}>
+    <div className={styles.category} style={{ background: `${background}` }}>
       <div className={styles.category_header}>
         <h1>{header}</h1>
         <BsArrowRightCircle></BsArrowRightCircle>
       </div>
-      <div className={styles.category_products}></div>
+      <div className={styles.category_products}>
+        {products.map((product, i) => (
+          <img key={i} src={product.image} alt="" />
+        ))}
+      </div>
     </div>
   );
 }
